0b5c53c37136fac306f65ce4baa7f66d19ae82a9
Eren-Yeager-80 <n.avinash0426@gmail.com>
2026-04-03 14:40:02 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+    import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+    import Header from './components/Header';
+    import Footer from './components/Footer';
+    import HomePage from './pages/HomePage';
+    import ShopPage from './pages/ShopPage';
+    import ProductDetailPage from './pages/ProductDetailPage';
+
+    function App() {
+      return (
+        <Router>
+          <div className="flex flex-col min-h-screen font-sans antialiased text-gray-900">
+            <Header />
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/shop" element={<ShopPage />} />
+                <Route path="/product/:id" element={<ProductDetailPage />} />
+                {/* Add other routes here as needed */}
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      );
+    }
+
+    export default App;
